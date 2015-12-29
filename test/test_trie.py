6801683dea24db5a9e852899dbc4bc4b6bbb1b4e
Philippe Ombredanne <pombredanne@nexb.com>
2015-12-29 18:58:05 +0100

    Cosmetics

--- test/test_trie.py
+++ test/test_trie.py
@@ -1,11 +1,34 @@
-from nose.tools import *
+#
+# Copyright (c) David Hain and Spideroak, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import trie
 
 
 def test_root_no_value():
     t = trie.Trie()
-    assert_raises(KeyError, t.__getitem__, ())
+    try:
+        t[()]
+    except KeyError:
+        pass
 
 
 def test_root_with_value():
@@ -22,27 +45,43 @@ def test_setitem():
 def test_needmore():
     t = trie.Trie()
     t['foo'] = 'bar'
-    assert_raises(trie.NeedMore, t.__getitem__, 'fo')
+    try:
+        t['fo']
+    except trie.NeedMore:
+        pass
 
 
 def test_keyerror():
     t = trie.Trie()
-    assert_raises(KeyError, t.__getitem__, 'foo')
+    try:
+        t['foo']
+    except  KeyError:
+        pass
 
 
 def test_delitem():
     t = trie.Trie()
     t['foo'] = 'bar'
     del t['foo']
-    assert_raises(KeyError, t.__getitem__, 'foo')
+
+    try:
+        t['foo']
+    except KeyError:
+        pass
     assert not t.root.nodes
 
 
 def test_delitem_keyerror():
     t = trie.Trie()
-    assert_raises(KeyError, t.__delitem__, 'foo')
+    try:
+        del t['foo']
+    except KeyError:
+        pass
     t['foobar'] = 'bar'
-    assert_raises(KeyError, t.__delitem__, 'foo')
+    try:
+        del t['foo']
+    except KeyError:
+        pass
 
 
 def test_delitem_node():
@@ -68,7 +107,7 @@ def test_iter():
     t['bar'] = 'barval'
     t['baz'] = 'bazval'
     itered = list(t)
-    assert itered == map(list, 'bar baz f foo'.split()), itered
+    assert itered == map(list, 'bar baz f foo'.split())
 
 
 def test_iteritems():
@@ -78,7 +117,7 @@ def test_iteritems():
     t['bar'] = 'barval'
     t['baz'] = 'bazval'
     itered = list(t.iteritems())
-    assert itered == zip(map(list, 'bar baz f foo'.split()), 'barval bazval fval fooval'.split()), itered
+    assert itered == zip(map(list, 'bar baz f foo'.split()), 'barval bazval fval fooval'.split())
 
 
 def test_itervalues():
@@ -88,7 +127,7 @@ def test_itervalues():
     t['bar'] = 'barval'
     t['baz'] = 'bazval'
     itered = list(t.itervalues())
-    assert itered == 'barval bazval fval fooval'.split(), itered
+    assert itered == 'barval bazval fval fooval'.split()
 
 
 def test_serialise():
@@ -103,7 +142,7 @@ def test_serialise():
     t['baz'] = 'bazval'
     t2 = pickle.loads(pickle.dumps(t))
     itered = list(t2.itervalues())
-    assert itered == 'barval bazval fval fooval'.split(), itered
+    assert itered == 'barval bazval fval fooval'.split()
 
     t = trie.Trie()
     t['obj'] = object()
